85fb70cce77436ad65dd480fa5b45cdeab9c42a2
sobold-marti <martina@soboldltd.com>
2025-01-13 09:13:10 +0000

    register menu

--- functions.php
+++ functions.php
@@ -202,3 +202,10 @@ add_action( 'graphql_register_types', function() {
         }
     ]);
 });
+
+function register_custom_menus() {
+    register_nav_menus(array(
+        'primary-menu' => __('Primary Menu'),
+    ));
+}
+add_action('after_setup_theme', 'register_custom_menus');
